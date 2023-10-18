a52210c3efc30bbc266f61e19cabc5b19e83cde1
Rajat Sharma <rajatxd007@gmail.com>
2023-10-18 07:54:49 +0530

    Added Codes

--- events.js
+++ events.js
@@ -13,6 +13,14 @@ celebrity.on('New Movie Launched' , ()=>{
     console.log("hey This Sunday watch my movie in threaters!!");
 });
 
+celebrity.on('New Movie',(data)=>{
+    console.log(`The New Movie for this Friday is ${data}`);
+});
+
+process.on('exit',(code)=>{
+    console.log('The Program has exited with code',code);
+} )
+
 celebrity.emit('New Movie Launched');
 celebrity.emit('Personal Trip');
-celebrity.emit('New Movie Launched');
+celebrity.emit('New Movie','Jawan');
